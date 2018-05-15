86316ba95ac9871bbbe467aea685f8561d52cc0e
Edyta Skibinska <edyta.skibinska@gmail.com>
2018-05-15 10:43:38 +0200

    displaying data finish

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -7,17 +7,27 @@ import { CoursesService } from 'src/app/courses.service';
   styleUrls: ['./course.component.css'],
   template: `
   <h2>{{ "Titre::"  +  getTitle() }}</h2>
+  <h2 [textContent]="title"></h2>
   <ul>
     <li *ngFor="let course of courses">
       {{ course }}
     </li>
   </ul>
+  <img src="{{ imageUrl }}"/>
+  <img [src]="imageUrl"/>
+  <table>
+    <tr>
+      <td [attr.colspan]="colSpan"></td>
+    </tr>
+  </table>
   `
 })
 export class CourseComponent implements OnInit {
 
 
   title = "List of courses";
+  imageUrl = "http://www.travelalerts.ca/wp-content/uploads/2014/03/Shallow-Waters.jpg";
+  colspan = 2;
  
   //logic calling a http services for this component
   //defining service un angular
