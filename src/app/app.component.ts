6854c570602ea5d146d49d02bd95acbc66f9a1fc
Edyta Skibinska <edyta.skibinska@gmail.com>
2018-05-15 16:15:10 +0200

    favourite reusable component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angularrr app ded';
+  
+  post = {
+    title: 'Angularrr app ded',
+    isFavourite: true
+  }
 }
