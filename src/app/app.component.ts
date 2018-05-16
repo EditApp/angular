ec4ee144b6f6d3c656ee5bc3e5f09bec2bc2b210
Edyta Skibinska <edyta.skibinska@gmail.com>
2018-05-16 11:46:51 +0200

    favourite reusable component - event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FavoriteChangedEventArgs } from 'src/app/favourite/favourite.component';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,7 @@ export class AppComponent {
     title: 'Angularrr app ded',
     isFavourite: true
   }
-  onFavouriteChanged(isFavourite) {
-    console.log('favourite is changed', isFavourite);
+  onFavouriteChanged(eventArgs: FavoriteChangedEventArgs) {
+    console.log('favourite is changed', eventArgs);
   }
 }
--- src/app/favourite/favourite.component.ts
+++ src/app/favourite/favourite.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class FavouriteComponent implements OnInit {
  @Input('isFavourite') isSelected: boolean;
- @Output() change = new EventEmitter()
+ @Output('change') click = new EventEmitter()
 
   constructor() { }
 
@@ -17,7 +17,13 @@ export class FavouriteComponent implements OnInit {
 
   onClick(){
     this.isSelected = !this.isSelected;
-    this.change.emit(this.isSelected);
+    this.click.emit({newValue: this.isSelected});
   }
 
 }
+
+
+export interface FavoriteChangedEventArgs {
+  newValue: boolean
+}
+
